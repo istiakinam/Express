cc64ae0b2823978b4d5e5b32f0df289ee7bdaa7e
Istiak Ahmed <istiak.inam@gmail.com>
2023-03-08 09:53:20 +0600

    added search items

--- index.js
+++ index.js
@@ -6,10 +6,20 @@ const logger = require('./middleware/logger');
 const app = express();
 
 //init middleware
-app.use(logger);
+//app.use(logger);
 
 //gets all children
 app.get('/api/children', (req, res) => res.json(children));
+
+//get single child
+app.get('/app/children/:id', (req, res) => {
+    const found = children.some(child => child.id === parseInt(req.params.id));
+    if(found) {
+        res.json(children.filter(child => child.id === parseInt(req.params.id )));
+    } else {
+        res.status(400).json({ msg: `No child with id of ${req.params.id}` });
+    }
+});
 
 //set static folder
 app.use(express.static(path.join(__dirname, 'files')));
